61d151d9f60d961ecf5ecff10a506aac17d02d93
catcadams <catceciliaadams@gmail.com>
2024-08-22 16:20:03 -0500

    part one of react exercises, movie, chores, and book lists

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,15 +1,18 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+  let pageTitle = "My favorite Books!";
+  let book1 =
+    "https://m.media-amazon.com/images/I/91EBMA21mAL._AC_UF1000,1000_QL80_.jpg";
+  let book2 =
+    "https://m.media-amazon.com/images/I/919qKnUFhuL._AC_UF1000,1000_QL80_.jpg";
+  let book3 =
+    "https://m.media-amazon.com/images/I/B1eAVSHxJ4L._AC_UF1000,1000_QL80_.jpg";
 
-   return (
-      <div>
-         <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
-      </div>      
-   );
+  return (
+    <div>
+      <h3>{pageTitle}</h3>
+      <img src={book1} alt="The Once and Future Witches by Alix E. Harrow" />
+      <img src={book2} alt="Sundial by Catriona Ward" />
+      <img src={book3} alt="Circe by Madeline Miller" />
+    </div>
+  );
 }
--- part1/exercises/src/components/ChoresList.jsx
+++ part1/exercises/src/components/ChoresList.jsx
@@ -1,3 +1,19 @@
-export default function ChoresList () {
-   return <p>No content is here!</p>;
+import React from "react";
+import classes from "./ChoresList.module.css";
+
+class ChoresList extends React.Component {
+  render() {
+    const chores = ["Sweep the floor", "Vacuum the stairs"];
+    return (
+      <div>
+        <h3 className={classes.choresHeading}>Today's Chores</h3>
+        <ol>
+          <li className={classes.choresText}>{chores[0]}</li>
+          <li className={classes.choresText}>{chores[1]}</li>
+        </ol>
+      </div>
+    );
+  }
 }
+
+export default ChoresList;
